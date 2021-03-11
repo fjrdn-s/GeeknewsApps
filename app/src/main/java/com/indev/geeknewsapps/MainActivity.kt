f3b956443ab8b30ac60ff44d9b253eec70215b25
fajarudin-it <fajar.dev1999@gmail.com>
2021-03-12 00:42:26 +0700

    Update buttonNavigation

--- app/src/main/java/com/indev/geeknewsapps/MainActivity.kt
+++ app/src/main/java/com/indev/geeknewsapps/MainActivity.kt
@@ -1,13 +1,33 @@
 package com.indev.geeknewsapps
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.Navigation
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val navView: BottomNavigationView = findViewById(R.id.nav_view)
 
 
+        // Passing each menu ID as a set of Ids because each
+        // menu should be considered as top level destinations.
+        val appBarConfiguration = AppBarConfiguration(
+            setOf(
+                R.id.navigation_home,
+                R.id.navigation_trending,
+                R.id.navigation_category,
+                R.id.navigation_search,
+                R.id.navigation_setting
+            )
+        )
+//        setupActionBarWithNavController(navController, appBarConfiguration)
+        val navController = Navigation.findNavController(this,R.id.nav_host_fragment)
+        NavigationUI.setupWithNavController(navView, navController)
     }
 }
--- app/src/main/java/com/indev/geeknewsapps/register/SuccessRegistActivity.kt
+++ app/src/main/java/com/indev/geeknewsapps/register/SuccessRegistActivity.kt
@@ -1,8 +1,8 @@
 package com.indev.geeknewsapps.register
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.indev.geeknewsapps.MainActivity
 import com.indev.geeknewsapps.R
 import kotlinx.android.synthetic.main.activity_success_regist.*
@@ -11,7 +11,7 @@ class SuccessRegistActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_success_regist)
-
+//
         btn_successRegist.setOnClickListener {
             val btnSuccessRegist = Intent(this@SuccessRegistActivity, MainActivity::class.java)
             startActivity(btnSuccessRegist)
