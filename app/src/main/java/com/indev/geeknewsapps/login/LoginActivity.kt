fe96b54f81957f9ef92806bbc4221731cd43f9f0
fajarudin-it <fajar.dev1999@gmail.com>
2021-03-29 00:33:27 +0700

    Updated : sign with Email & Password, edited : algorithm intent if user is login

--- app/src/main/java/com/indev/geeknewsapps/login/LoginActivity.kt
+++ app/src/main/java/com/indev/geeknewsapps/login/LoginActivity.kt
@@ -3,6 +3,8 @@ package com.indev.geeknewsapps.login
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.util.Patterns
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.android.gms.auth.api.signin.GoogleSignIn
 import com.google.android.gms.auth.api.signin.GoogleSignInClient
@@ -12,6 +14,7 @@ import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.GoogleAuthProvider
 import com.indev.geeknewsapps.R
 import com.indev.geeknewsapps.register.RegisterActivity
+import com.indev.geeknewsapps.register.SuccessRegistActivity
 import com.indev.geeknewsapps.register.UserProfilingActivity
 import kotlinx.android.synthetic.main.activity_login.*
 
@@ -32,18 +35,18 @@ class LoginActivity : AppCompatActivity() {
 
         // Configure Google Sign In
         val gso = GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-            .requestIdToken(getString(R.string.default_web_client_id))
-            .requestEmail()
-            .build()
+                .requestIdToken(getString(R.string.default_web_client_id))
+                .requestEmail()
+                .build()
         googleSignInClient = GoogleSignIn.getClient(this, gso)
 
 
-//        btn_login.setOnClickListener {
-//
-//        }
+        btn_login.setOnClickListener {
+            signInWithEmail()
+        }
 
         btn_gmail.setOnClickListener {
-            signIn()
+            signInWithGoogle()
         }
 
         tv_regist.setOnClickListener {
@@ -52,7 +55,46 @@ class LoginActivity : AppCompatActivity() {
         }
     }
 
-    private fun signIn() {
+    private fun signInWithEmail() {
+        val email = et_emailAddress.text.toString().trim()
+        val password = et_password.text.toString().trim()
+
+        if (email.isEmpty()) {
+            et_emailAddress.error = "Please enter email address"
+            et_emailAddress.requestFocus()
+            return
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            et_emailAddress.error = "Please enter email address"
+            et_emailAddress.requestFocus()
+            return
+        }
+
+        if (password.isEmpty()) {
+            et_password.error = "Please enter password"
+            et_password.requestFocus()
+            return
+        }
+
+        signInUser(email, password)
+    }
+
+    private fun signInUser(email: String, password: String) {
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) {
+                    if (it.isSuccessful) {
+                        val successIntent = Intent(this, SuccessRegistActivity::class.java)
+                        startActivity(successIntent)
+                        finish()
+                    } else {
+                        Toast.makeText(this, "${it.exception?.message}", Toast.LENGTH_SHORT).show()
+//                        Log.w("LoginActivity", "Google sign in failed")
+                    }
+                }
+    }
+
+    private fun signInWithGoogle() {
         val signInIntent = googleSignInClient.signInIntent
         startActivityForResult(signInIntent, RC_SIGN_IN)
     }
@@ -83,17 +125,17 @@ class LoginActivity : AppCompatActivity() {
     private fun firebaseAuthWithGoogle(idToken: String) {
         val credential = GoogleAuthProvider.getCredential(idToken, null)
         mAuth.signInWithCredential(credential)
-            .addOnCompleteListener(this) { task ->
-                if (task.isSuccessful) {
-                    // Sign in success, update UI with the signed-in user's information
-                    Log.d("LoginActivity", "signInWithCredential:success")
-                    val profillingIntent = Intent(this, UserProfilingActivity::class.java)
-                    startActivity(profillingIntent)
-                    finish()
-                } else {
-                    // If sign in fails, display a message to the user.
-                    Log.w("LoginActivity", "signInWithCredential:failure", task.exception)
+                .addOnCompleteListener(this) { task ->
+                    if (task.isSuccessful) {
+                        // Sign in success, update UI with the signed-in user's information
+                        Log.d("LoginActivity", "signInWithCredential:success")
+                        val profillingIntent = Intent(this, UserProfilingActivity::class.java)
+                        startActivity(profillingIntent)
+                        finish()
+                    } else {
+                        // If sign in fails, display a message to the user.
+                        Log.w("LoginActivity", "signInWithCredential:failure", task.exception)
+                    }
                 }
-            }
     }
 }
--- app/src/main/java/com/indev/geeknewsapps/register/RegisterActivity.kt
+++ app/src/main/java/com/indev/geeknewsapps/register/RegisterActivity.kt
@@ -3,25 +3,127 @@ package com.indev.geeknewsapps.register
 import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
+import android.util.Patterns
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.firebase.auth.FirebaseAuth
 import com.indev.geeknewsapps.R
 import kotlinx.android.synthetic.main.activity_register.*
 import kotlinx.android.synthetic.main.toolbar_layout.*
 
 class RegisterActivity : AppCompatActivity() {
+
+//    companion object {
+//        const val REQUEST_CAMERA = 100
+//    }
+//
+//    private lateinit var imageUri: Uri
+    private lateinit var mAuth: FirebaseAuth
+
     @SuppressLint("UseCompatLoadingForDrawables")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        mAuth = FirebaseAuth.getInstance()
 
         toolbar_category.navigationIcon = resources.getDrawable(R.drawable.ic_arrow_back_black, null)
         toolbar_category.setNavigationOnClickListener {
             onBackPressed()
         }
 
-        ib_regist.setOnClickListener {
-            val ibRegist = Intent(this, UserProfilingActivity::class.java)
-            startActivity(ibRegist)
+//        iv_profile.setOnClickListener {
+//            requestCamera()
+//        }
+
+        btn_regist.setOnClickListener {
+            requestRegist()
         }
+    }
+
+//    private fun requestCamera() {
+//        Intent(MediaStore.ACTION_IMAGE_CAPTURE).also { intent ->
+//            this.packageManager?.let {
+//                intent.resolveActivity(it).also {
+//                    startActivityForResult(intent, REQUEST_CAMERA)
+//                }
+//            }
+//        }
+//    }
+//
+//    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+//        super.onActivityResult(requestCode, resultCode, data)
+//        if (requestCode == REQUEST_CAMERA && resultCode == RESULT_OK) {
+//            val imgBitmap = data?.extras?.get("data") as Bitmap
+//            uploadImage(imgBitmap)
+//        }
+//    }
+//
+//    private fun uploadImage(imgBitmap: Bitmap) {
+//        val baos = ByteArrayOutputStream()
+//        val reference = FirebaseStorage.getInstance()
+//                .reference
+//            .child("img/")
+////                .child("img/${FirebaseAuth.getInstance().currentUser?.uid}")
+//
+//        imgBitmap.compress(Bitmap.CompressFormat.JPEG, 100, baos)
+//        val image = baos.toByteArray()
+//
+//        reference.putBytes(image)
+//                .addOnCompleteListener { task ->
+//                    if (task.isSuccessful) {
+//                        reference.downloadUrl.addOnCompleteListener { Uri ->
+//                            Uri.result?.let {
+//                                imageUri = it
+//                                iv_profile.setImageBitmap(imgBitmap)
+//                            }
+//                        }
+//                    }
+//                }
+//    }
+
+    private fun requestRegist() {
+        val fullName = et_fullName.text.toString().trim()
+        val email = et_emailAddress.text.toString().trim()
+        val password = et_password.text.toString().trim()
+
+        if (fullName.isEmpty()) {
+            et_fullName.error = "Please enter full name!"
+            et_fullName.requestFocus()
+            return
+        }
+
+        if (email.isEmpty()) {
+            et_emailAddress.error = "Please enter email address!"
+            et_emailAddress.requestFocus()
+            return
+        }
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+            et_emailAddress.error = "Invalid email!"
+            et_emailAddress.requestFocus()
+            return
+        }
+
+        if (password.isEmpty() || password.length < 6) {
+            et_password.error = "Password must between 6 to 16 character"
+            et_password.requestFocus()
+            return
+        }
+
+        registerUser(email, password)
+    }
+
+    private fun registerUser(email: String, password: String) {
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this) {
+                    if (it.isSuccessful) {
+                        val profillingIntent = Intent(this, UserProfilingActivity::class.java)
+                        startActivity(profillingIntent)
+                        finish()
+                    } else {
+                        Toast.makeText(this, it.exception?.message, Toast.LENGTH_SHORT).show()
+                    }
+                }
     }
 }
